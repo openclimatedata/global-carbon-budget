1fe04471257adff17851e50022fc64232b818708
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2017-09-20 11:56:16 +0200

    Enable primary index validation
    Fixed in Goodtables 1.4.2

--- scripts/validate.py
+++ scripts/validate.py
@@ -6,7 +6,6 @@ from goodtables.cli import _print_report
 report = validate(
     str(root / "datapackage.json"),
     table_limit=20,
-    row_limit=20000,
-    skip_checks=["unique-constraint"]
+    row_limit=20000
 )
 _print_report(report)
