c241994a10b01fe9461d78333d67ed4d21b98dad
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2017-11-14 17:08:06 +0100

    Use country codes for consumption emissions

--- scripts/consumption-emissions.py
+++ scripts/consumption-emissions.py
@@ -1,8 +1,7 @@
-import numpy as np
 import pandas as pd
 
 from util import root, excel_national
-
+from countrynames import to_code_3
 
 consumption_emissions_csv = root / "data/consumption-emissions.csv"
 
@@ -10,24 +9,30 @@ consumption_emissions_csv = root / "data/consumption-emissions.csv"
 consumption_emissions = pd.read_excel(
     excel_national,
     sheet_name="Consumption Emissions GCB",
-    skiprows=7,
-    index_col=0,
-    header=[0, 1]
+    skiprows=8,
+    index_col=0
 )
 consumption_emissions.index.name = "Year"
 consumption_emissions = consumption_emissions.T
-consumption_emissions.index.rename(["CDIAC-Name", "Name"], inplace=True)
+consumption_emissions.index.rename("Name", inplace=True)
+
+consumption_emissions["Code"] = [to_code_3(i) or i
+                                 for i in consumption_emissions.index]
+
+consumption_emissions = consumption_emissions.reset_index().drop(
+    "Name", axis=1)
 
 consumption_emissions = pd.melt(
-    consumption_emissions.reset_index(),
-    id_vars=['Name', 'CDIAC-Name'],
+    consumption_emissions,
+    id_vars="Code",
     var_name="Year",
     value_name="Consumption-Emissions"
 )
 
+consumption_emissions.sort_values(["Code", "Year"], inplace=True)
+
 consumption_emissions.dropna(inplace=True)
 
-consumption_emissions.sort_values(["Name", "Year"], inplace=True)
 consumption_emissions.to_csv(
     consumption_emissions_csv,
     encoding="UTF-8",
