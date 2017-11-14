cdf98936d6fa0665d35fca040b5b5236885e95ee
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2017-11-14 17:11:23 +0100

    Use country codes for emissions transfers

--- scripts/emissions-transfers.py
+++ scripts/emissions-transfers.py
@@ -1,7 +1,7 @@
-import numpy as np
 import pandas as pd
 
 from util import root, excel_national
+from countrynames import to_code_3
 
 emissions_transfers_csv = root / "data/emissions-transfers.csv"
 
@@ -9,24 +9,30 @@ emissions_transfers_csv = root / "data/emissions-transfers.csv"
 emissions_transfers = pd.read_excel(
     excel_national,
     sheet_name="Emissions Transfers GCB",
-    skiprows=7,
-    index_col=0,
-    header=[0, 1]
+    skiprows=8,
+    index_col=0
 )
 emissions_transfers.index.name = "Year"
 emissions_transfers = emissions_transfers.T
-emissions_transfers.index.rename(["CDIAC-Name", "Name"], inplace=True)
+emissions_transfers.index.rename("Name", inplace=True)
+
+emissions_transfers["Code"] = [to_code_3(i) or i
+                               for i in emissions_transfers.index]
+
+emissions_transfers = emissions_transfers.reset_index().drop(
+    "Name", axis=1)
+
 
 emissions_transfers = pd.melt(
-    emissions_transfers.reset_index(),
-    id_vars=['Name', 'CDIAC-Name'],
+    emissions_transfers,
+    id_vars="Code",
     var_name="Year",
     value_name="Emissions-Transfers"
 )
 
+emissions_transfers.sort_values(["Code", "Year"], inplace=True)
 emissions_transfers.dropna(inplace=True)
 
-emissions_transfers.sort_values(["Name", "Year"], inplace=True)
 emissions_transfers.to_csv(
     emissions_transfers_csv,
     encoding="UTF-8",
