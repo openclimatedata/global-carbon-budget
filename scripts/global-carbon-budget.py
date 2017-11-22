1bbfba936c74a7169c693134a15e5d970d1f4fae
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2017-11-22 16:09:14 +0100

    Add 'Global Carbon Budget' sheet as CSV file

---
+++ scripts/global-carbon-budget.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+from util import root, excel_global
+
+gcb_csv = root / "data/global-carbon-budget.csv"
+
+# Global Carbon Budget
+
+gcb = pd.read_excel(
+    excel_global,
+    sheet_name="Global Carbon Budget",
+    skiprows=19,
+    index_col="Year"
+)
+
+gcb.columns = [i.title().replace(" ", "-") for i in gcb.columns]
+
+gcb.to_csv(
+    gcb_csv,
+    encoding="UTF-8",
+    float_format="%.3f"
+)
