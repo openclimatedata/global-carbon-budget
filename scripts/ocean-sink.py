d01fcdf178742276fd11ca84f2d4af3f3832d5c0
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2017-11-22 15:52:47 +0100

    Fix Ocean Sink mean calculation
    The original Excel file was missing a colum in the
    formula definition. (Closes #3)

--- scripts/ocean-sink.py
+++ scripts/ocean-sink.py
@@ -18,6 +18,11 @@ ocean_sink.index.name = "Year"
 
 ocean_sink.rename(columns={"GCB": "Ocean-Sink"}, inplace=True)
 
+# Fix the GCB column, as the last column was not included in the original Excel
+# calculation in v1.1 (#1).
+ocean_sink["Ocean-Sink"] = ocean_sink.loc[:, "CCSM-BEC":"NorESM-OC"].mean(
+    axis=1)
+
 ocean_sink.to_csv(
     ocean_sink_csv,
     encoding="UTF-8",
