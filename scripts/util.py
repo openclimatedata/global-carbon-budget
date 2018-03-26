b465794d3f292792ac10a79ebdb69ded1015b82e
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2018-03-26 11:27:36 +0200

    Update National Excel file to 1.2
    Includes no changes.

--- scripts/util.py
+++ scripts/util.py
@@ -3,4 +3,4 @@ from pathlib import Path
 root = Path(__file__).parents[1]
 
 excel_global = root / "archive/Global_Carbon_Budget_2017v1.3.xlsx"
-excel_national = root / "archive/National_Carbon_Emissions_2017v1.1.xlsx"
+excel_national = root / "archive/National_Carbon_Emissions_2017v1.2.xlsx"
