2e601d8545785c7db19fa7b8d26fbfa6507e57a7
Robert Gieseke <rob.g@web.de>
2026-05-07 17:25:11 +0200

    Add new generation script using `openclimatedata` library

---
+++ scripts/generate.py
@@ -0,0 +1,98 @@
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+#     "openclimatedata>=0.33",
+# ]
+# ///
+
+from pathlib import Path
+
+import openclimatedata as ocd
+
+root = Path(__file__).parents[1]
+
+
+print(f"openclimatedata, version {ocd.__version__}")
+
+single_tables = [
+    {"sheet_name": "Global Carbon Budget", "slug": "global-carbon-budget"},
+    {"sheet_name": "Historical Budget", "slug": "historical-budget"},
+    {"sheet_name": "Fossil Emissions", "slug": "fossil-emissions"},
+    {"sheet_name": "Cement Carbonation Sink", "slug": "cement-carbonation"},
+]
+
+subtables = [
+    {"sheet_name": "Land-Use Change Emissions", "slug": "land-use-change-emissions"},
+    {"sheet_name": "Ocean Sink", "slug": "ocean-sink"},
+    {"sheet_name": "Terrestrial Sink", "slug": "terrestrial-sink"},
+]
+
+
+def comment_notes(notes):
+    commented_notes = "\n".join([f"# {line}" for line in notes.split("\n")])
+    commented_notes += f"\n# Generated with openclimatedata {ocd.__version__} from the GCB Excel file from {ocd.Global_Carbon_Budget[version].doi}"
+    commented_notes += f"\n# http://openclimatedata.net - https://github.com/openclimatedata/global-carbon-budget"
+    commented_notes += "\n"
+    return commented_notes
+
+
+for version in ocd.Global_Carbon_Budget.keys():
+    print(f"Release Global Carbon Budget {version}")
+
+    for item in single_tables:
+        sheet_name = item["sheet_name"]
+        slug = item["slug"]
+        if (slug == "cement-carbonation") and (int(version) <= 2019):
+            continue
+
+        if slug == "fossil-emissions":
+            if int(version) <= 2019:
+                sheet_name += " by Fuel Type"
+            else:
+                sheet_name += " by Category"
+
+        print(f"Sheet: {sheet_name}")
+
+        notes = ocd.Global_Carbon_Budget[version][sheet_name].__repr__()
+        commented_notes = comment_notes(notes)
+        csv_data = ocd.Global_Carbon_Budget[version][sheet_name].to_dataframe().to_csv()
+
+        filepath = f"data/global-carbon-budget-{version}-{slug}.csv"
+        print(filepath)
+
+        with open(root / filepath, "w") as f:
+            f.write(commented_notes)
+            f.write(csv_data)
+
+    for item in subtables:
+        sheet_name = item["sheet_name"]
+        slug = item["slug"]
+
+        print(f"Sheet: {sheet_name}")
+
+        notes = ocd.Global_Carbon_Budget[version][sheet_name].__repr__()
+        commented_notes = comment_notes(notes)
+
+        for subtable in ocd.Global_Carbon_Budget[version][sheet_name]:
+            csv_data = (
+                ocd.Global_Carbon_Budget[version][sheet_name][subtable]
+                .to_dataframe()
+                .to_csv()
+            )
+            subtable_slug = (
+                subtable.lower()
+                .replace(" ", "-")
+                .replace("&", "")
+                .replace("(", "")
+                .replace(")", "")
+                .replace("---", "-")
+                .replace("--", "-")
+                .replace("-net-does-not-include-peat-emissions", "")
+            )
+            filepath = f"data/global-carbon-budget-{version}-{slug}-{subtable_slug}.csv"
+            print(filepath)
+
+            with open(root / filepath, "w") as f:
+                f.write(commented_notes)
+                f.write(f"#\n# {subtable}\n")
+                f.write(csv_data)
