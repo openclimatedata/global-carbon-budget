01af18091a4018c16b4877ff7329d8fb96c6cfc1
Robert Gieseke <rob.g@web.de>
2026-05-07 17:27:09 +0200

    Remove trailing whitespace

--- scripts/generate.py
+++ scripts/generate.py
@@ -29,7 +29,7 @@ subtables = [
 
 
 def comment_notes(notes):
-    commented_notes = "\n".join([f"# {line}" for line in notes.split("\n")])
+    commented_notes = "\n".join([f"# {line}".strip() for line in notes.split("\n")])
     commented_notes += f"\n# Generated with openclimatedata {ocd.__version__} from the GCB Excel file from {ocd.Global_Carbon_Budget[version].doi}"
     commented_notes += f"\n# http://openclimatedata.net - https://github.com/openclimatedata/global-carbon-budget"
     commented_notes += "\n"
